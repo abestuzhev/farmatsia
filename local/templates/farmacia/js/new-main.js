074ad94c9b6b8627b7a204c8c08b747cde9ef470
Aleksey <aleksey.bestuzhev@gmail.com>
2017-04-19 01:07:49 +0300

    changes

--- local/templates/farmacia/js/new-main.js
+++ local/templates/farmacia/js/new-main.js
@@ -1,3 +1,10 @@
 ;$(document).ready(function() {
     $('input[type="file"]').imageuploadify();
+
+    $('.application-item_header').on('click', function(){
+        //remove siblings
+        $(this).parents('.application-item ').siblings().removeClass('application-item--active');
+
+        $(this).parents('.application-item').toggleClass('application-item--active');
+    });
 });
