c2d19081a471421ad9200431342605668c17a483
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-04-19 12:03:51 +0300

    changes

--- local/templates/farmacia/js/new-main.js
+++ local/templates/farmacia/js/new-main.js
@@ -4,7 +4,35 @@
     $('.application-item_header').on('click', function(){
         //remove siblings
         $(this).parents('.application-item ').siblings().removeClass('application-item--active');
-
         $(this).parents('.application-item').toggleClass('application-item--active');
     });
+
+
+    $('.js-apply_button').click(function (e) {
+        e.preventDefault();
+        sendFullForm();
+    });
+
+    function sendFullForm() {
+
+      function checkNames (input){
+        var val = input.val();
+        if (val.length <= 0 || val.length >= 50) {
+            $(input).siblings('label').css("color", "#CB3631");
+            resultat = false;
+        } else {
+          $(input).siblings('label').css("color", "inherit");
+        }
+      } // checkNames
+
+      checkNames($('#pharmacies-number'));
+      checkNames($('#apply-user'));
+      checkNames($('#apply-phone-number'));
+
+    };// sendFullForm
+
+    // mask
+    $('#apply-phone-number').mask('+7(000)000-00-00');
+    $('#pharmacies-number').mask('0000');
+
 });
