75c927cc3f6dd3e928578e20fa482ee24307238b
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-04-21 15:06:35 +0300

    changes

--- local/templates/farmacia/js/new-main.js
+++ local/templates/farmacia/js/new-main.js
@@ -15,7 +15,7 @@
 
     function sendFullForm() {
 
-      function checkNames (input){
+      function checkLabel (input){
         var val = input.val();
         if (val.length <= 0 || val.length >= 50) {
             $(input).siblings('label').css("color", "#CB3631");
@@ -25,14 +25,28 @@
         }
       } // checkNames
 
-      checkNames($('#pharmacies-number'));
-      checkNames($('#apply-user'));
-      checkNames($('#apply-phone-number'));
+      function checkBorder (input){
+        var val = input.val();
+        if (val.length <= 0 || val.length >= 50) {
+            $(input).css("border-color", "#CB3631");
+            resultat = false;
+        } else {
+          $(input).css("border-color", "inherit");;
+        }
+      } // checkBorder
+
+      checkLabel($('#pharmacies-number'));
+      checkLabel($('#apply-user'));
+      checkLabel($('#apply-phone-number'));
+      checkBorder($('#guest-book_email'));
 
     };// sendFullForm
 
+
+
     // mask
     $('#apply-phone-number').mask('+7(000)000-00-00');
+    $('#guest-book_phone').mask('+7(000)000-00-00');
     $('#pharmacies-number').mask('0000');
 
 });
