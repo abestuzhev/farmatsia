5eb5b45c2cc177139a0095381965db848d99f7e3
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-04-24 10:38:45 +0300

    changes

--- local/templates/farmacia/js/new-main.js
+++ local/templates/farmacia/js/new-main.js
@@ -1,13 +1,15 @@
 ;$(document).ready(function() {
+    //загрузка файла
     $('input[type="file"]').imageuploadify();
 
+    //история заявок
     $('.application-item_header').on('click', function(){
         //remove siblings
         $(this).parents('.application-item ').siblings().removeClass('application-item--active');
         $(this).parents('.application-item').toggleClass('application-item--active');
     });
 
-
+    //валидация при отправке в форме apply
     $('.js-apply_button--apply').click(function (e) {
         e.preventDefault();
         sendFullFormApply();
@@ -28,7 +30,7 @@
       checkLabel($('#apply-phone-number'));
     };// sendFullForm
 
-
+    //валидация при отправке в форме guest
     $('.js-apply_button--guest').click(function (e) {
         e.preventDefault();
         sendFullFormGuest();
@@ -45,57 +47,20 @@
         }
       } // checkBorder
 
+
+      //изменения инпута телефон формы guest
       var $guestBookPhone = $('#guest-book_phone');
-
-
-      //изменения инпутов телефон и email
       $guestBookPhone.on('change', function(){
-        var val = $(this).val();
-        if (val.length <= 0 || val.length >= 50) {
-            $(this).css("border-color", "#CB3631");
-        } else {
-          $(this).css("border-color", "#b2b2b2");
           $('#guest-book_email').css("border-color", "#b2b2b2");
-        }
       });
 
       if($guestBookPhone.val() <= 0) {
         checkBorder($('#guest-book_email'));
       }
-
-      // $('#guest-book_email').on('change', function(){
-      //   checkBorder($('#guest-book_email'));
-      //   $('#guest-book_phone').css("border-color", "inherit !important");
-      // });
-
-
-      // checkBorder($('#guest-book_phone'));
     }
-
-
-
-
-    // function addDisabled (inputChange, inputDisabled){
-    //   inputChange.on('change', function(){
-    //     var val = $(this).val();
-    //     if(val.length > 0){
-    //       inputDisabled.attr('disabled','disabled');
-    //     }else if(val.length == 0){
-    //       inputDisabled.removeAttr('disabled');
-    //     }
-    //   })
-    // }
-
-
-
-    // addDisabled(  $('#guest-book_phone'), $("#guest-book_email"));
-    // addDisabled(  $("#guest-book_email"), $('#guest-book_phone'));
-
-
 
     // mask
     $('#apply-phone-number').mask('+7(000)000-00-00');
     $('#guest-book_phone').mask('+7(000)000-00-00');
     $('#pharmacies-number').mask('0000');
-
 });
